53d2041392c85f59990ca23ecbf9c5d3c26ffbc1
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-07 17:02:04 +0300

    added the profile path

--- app/main/views.py
+++ app/main/views.py
@@ -14,4 +14,12 @@ def index():
     title = 'Home'
     return render_template('index.html', title = title)
 
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
