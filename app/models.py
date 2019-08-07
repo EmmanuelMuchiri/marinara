c5b3894edd4d4464a8718ac7d5ebf7e56d32b174
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-07 17:14:38 +0300

    added class Admin with table admin to achieve functionality for login as an admin so as to view pizza orders

--- app/models.py
+++ app/models.py
@@ -29,3 +29,12 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
